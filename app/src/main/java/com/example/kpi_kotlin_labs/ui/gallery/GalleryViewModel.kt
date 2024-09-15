6adb7d124d302fecb993b7cabddb439e30ba866e
Mark Hudzovskyi <markgud1717@gmail.com>
2024-09-15 12:16:12 -0400

    feat: implemented task 1 for lab1

---
+++ app/src/main/java/com/example/kpi_kotlin_labs/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.kpi_kotlin_labs.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
